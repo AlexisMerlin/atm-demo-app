bb1ddb0b645620890520a9f60d68e568f839e5bf
aaldana@procomconsulting.com <aldanam.alexis@gmail.com>
2025-04-04 18:01:56 -0600

    Add main menu user interaction playwrigt tests

--- tests/atm-app.spec.ts
+++ tests/atm-app.spec.ts
@@ -36,4 +36,58 @@ test.describe('ATM interface', () => {
 
     await expect(page.locator('text=Hi Mary Jane Watson!')).toBeVisible();
   });
+
+  test.describe('Main Menu Interaction', () => {
+    test.beforeEach(async ({ page }) => {
+      // Enter PIN before each interaction
+      await page.getByTestId('atm-button-btn-8').click();//Enter Pin button
+      await page.locator('button:text("1")').click();
+      await page.locator('button:text("2")').click();
+      await page.locator('button:text("3")').click();
+      await page.locator('button:text("4")').click();
+      await page.getByRole('button', { name: 'Enter' }).click();
+    });
+
+    test('Shoud show balance', async ({ page }) => {
+      await expect(page.locator('text=Hi Peter Parker!')).toBeVisible();
+
+      await page.getByTestId('atm-button-btn-6').click(); //balance button
+      await expect(page.locator('text=debit card')).toBeVisible();
+      await expect(page.locator('text=Your current balance is: $0.00')).toBeVisible();
+
+      await page.getByTestId('atm-button-btn-8').click();
+      await expect(page.locator('text=Hi Peter Parker!')).toBeVisible();
+    });
+
+    test('Shoud show error on withdraw', async ({ page }) => {
+      await expect(page.locator('text=Hi Peter Parker!')).toBeVisible();
+
+      await page.getByTestId('atm-button-btn-5').click(); //withdraw button
+      await expect(page.locator('text=Enter amount to withdraw')).toBeVisible();
+      await page.locator('button:text("1")').click();
+      await page.locator('button:text("0")').click();
+      await page.locator('button:text("0")').click();
+      await page.locator('button:text("0")').click();
+      await expect(page.locator('text=10.00')).toBeVisible();
+      await page.getByRole('button', { name: 'Enter' }).click();
+      await expect(page.locator('text=Insufficient funds')).toBeVisible(); // :(
+    });
+
+    test('Shoud be Able to Deposit', async ({ page }) => {
+      await expect(page.locator('text=Hi Peter Parker!')).toBeVisible();
+
+      await page.getByTestId('atm-button-btn-7').click(); //deposit button
+      await expect(page.locator('text=Enter amount to deposit')).toBeVisible();
+
+      await page.locator('button:text("1")').click();
+      await page.locator('button:text("0")').click();
+      await page.locator('button:text("0")').click();
+      await page.locator('button:text("0")').click();
+      await page.locator('button:text("0")').click();
+      await expect(page.locator('text=100.00')).toBeVisible();
+      await page.getByRole('button', { name: 'Enter' }).click();
+
+      await expect(page.locator('text=Your current balance is: $100.00')).toBeVisible(); // :(
+    });
+  });
 });
